0fde6ad2213c4738de7bb866edf6f6d596e11aa9
AndiH <a.herten+github@gmail.com>
2016-03-30 19:28:43 +0200

    Move over to adding dices by reference of a def

--- Blocks/drawing.py
+++ Blocks/drawing.py
@@ -25,27 +25,27 @@ documentHeight = nDicePerRow * 1.6 * basesize
 svg_document = svgwrite.Drawing(filename="drawing.svg", size=(str(documentWidth) + "px", str(documentHeight) + "px"))
 svg_document.add(svg_document.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(50,50,50)'))
 
-rowofdice = svg_document.g()
+# GENERATE BASE DICES
+for c in allColors:
+    dice = createDiceOneColor(svg_document, basesize, angle, c)
+    dice['id'] = c[1:]
+    svg_document.defs.add(dice)
+
+# GENERATE DICES (as references)
+allDices = svg_document.g(id="allthedices")
 for (i, j) in itertools.product(range(nDicePerRow), range(nDicePerRow)):
     randomnumber = np.random.triangular(0, 0, nDicePerRow)
     colors = colors_andrea
     if (i <= randomnumber):
         colors = colors_andi
     random.shuffle(colors)
-    # dice = createDice(svg_document, colors[0], colors[1], colors[2])
-    dice = createDiceOneColor(svg_document, basesize, angle, colors[0])
     offset = -basesize if (j % 2 == 1) else 0
+    dice = svg_document.use(svg_document.g(id=colors[0][1:]))
     dice.translate(tx=offset + i * 2 * basesize, ty=j * basesize * (1 + math.tan(math.radians(angle))))
-    rowofdice.add(dice)
+    allDices.add(dice)
 
-# rowofdice2 = rowofdice.copy()
-# rowofdice2.translate(tx = -basesize, ty = basesize * (1 + math.tan(math.radians(angle))))
-# rowofdice3 = rowofdice.copy()
-# rowofdice3.translate(tx = 0, ty = 2 * basesize * (1 + math.tan(math.radians(angle))))
+svg_document.add(allDices)
 
-svg_document.add(rowofdice)
-# svg_document.add(rowofdice2)
-# svg_document.add(rowofdice3)
 
 helloWorld = svg_document.text("Hello World", insert=("50%", "50%"), style="font-size: {}px; font-family: PT Sans; font-weight: bolder;".format(basesize * 3), fill="white")
 helloWorld['text-anchor'] = "middle"
