bc013679524d8dd1dbf210e22a37ba265ed571d5
AndiH <a.herten+github@gmail.com>
2016-05-01 18:30:35 +0200

    Make size of document a parameter, let it fill with dices
    Parameter is set to 400 currently

--- Blocks/drawing.py
+++ Blocks/drawing.py
@@ -11,6 +11,14 @@ from dice import createDiceOneColor
 from helpers import readParams
 
 
+def mmtopx(mm):
+    return mm*3.543
+
+
+def pxtomm(px):
+    return px/3.543
+
+
 params = readParams("../params.toml")
 angle = params["dice"]["viewangle"]
 basesize = params["dice"]["basesize"]
@@ -18,12 +26,16 @@ colors_andi = params["colors"]["ah"]
 colors_andrea = params["colors"]["aw"]
 allColors = colors_andi + colors_andrea
 
-nDicePerRow = 20
 
-documentWidth = nDicePerRow * 2 * basesize
-documentHeight = nDicePerRow * 1.6 * basesize
-svg_document = svgwrite.Drawing(filename="drawing.svg", size=(str(documentWidth) + "px", str(documentHeight) + "px"))
-# svg_document.add(svg_document.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(50,50,50)'))
+# documentWidth = nDicePerRow * 2 * basesize
+documentWidth = (400, "mm")
+# documentHeight = nDicePerRow * 1.6 * basesize
+documentHeight = (100, "mm")
+svg_document = svgwrite.Drawing(filename="drawing.svg", size=("{}{}".format(documentWidth[0], documentWidth[1]), "{}{}".format(documentHeight[0], documentHeight[1])))
+svg_document.add(svg_document.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(50,50,50)'))
+
+nDicePerRow = documentWidth[0]/pxtomm(basesize)/2+6  # wtf!
+nDicePerColumn = documentHeight[0]/pxtomm(basesize)/2+7  # tf!
 
 # GENERATE BASE DICES
 for c in allColors:
@@ -37,8 +49,8 @@ squareClip.add(svg_document.rect((0, 0), ("100%", "100%")))
 
 # GENERATE DICES (as references)
 allDices = svg_document.g(id="allthedices",  clip_path='url(#squareClip)')
-for (i, j) in itertools.product(range(nDicePerRow + 1), range(nDicePerRow + 1)):
-    randomnumber = np.random.triangular(0, 0, nDicePerRow)
+for (i, j) in itertools.product(range(int(nDicePerRow) + 1), range(int(nDicePerColumn) + 1)):
+    randomnumber = np.random.triangular(nDicePerRow/4, nDicePerRow/4, 3*nDicePerRow/4)
     colors = colors_andrea
     if (i <= randomnumber):
         colors = colors_andi
@@ -61,7 +73,7 @@ firstAcolor = random.choice(params["monogram"]["Aone_colors"])
 secondAcolor = random.choice(params["monogram"]["Atwo_colors"])
 firstA = svgwrite.path.Path(params["monogram"]["Aone"], fill=firstAcolor, stroke="none")
 secondA = svgwrite.path.Path(params["monogram"]["Atwo"], fill=secondAcolor, stroke="none")
-monogram = svg_document.g(id="monogram", transform="translate(-20,-60)")
+monogram = svg_document.g(id="monogram", transform="translate({},-40)".format(mmtopx(documentWidth[0])/2 - 278/2))
 monogram.add(firstA)
 monogram.add(secondA)
 svg_document.add(monogram)
